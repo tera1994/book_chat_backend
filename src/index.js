08e891a841d0a5ee103832140a4bad0d51c2ea5e
tera <>
2024-03-19 15:51:34 +0900

    Format file.

--- src/index.js
+++ src/index.js
@@ -1,100 +1,124 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 
-const { Server } = require('socket.io');
-const connectDB = require('./database');
-const { bookModel } = require('./model');
+const { Server } = require("socket.io");
+const connectDB = require("./database");
+const { bookModel } = require("./model");
 
-const port = '5000';
+const port = process.env.PORT || "5000";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const cors = require("cors")
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-app.post('/book-chat-room', async (req, res) => {
-    try {
-        await connectDB();
-        await bookModel.create(req.body);
-    } catch (error) {
-        console.log(`ERROR : ${error.message}`)
-        return res.status(400).json({ message: "FAILURE : Create a book chat room." })
-    }
+app.post("/book-chat-room", async (req, res) => {
+  try {
+    await connectDB();
+    await bookModel.create(req.body);
+  } catch (error) {
+    console.log(`ERROR : ${error.message}`);
+    return res
+      .status(400)
+      .json({ message: "FAILURE : Create a book chat room." });
+  }
 
-    return res.status(200).json({ message: "SUCCESS: Create a book chat room." })
+  return res.status(200).json({ message: "SUCCESS: Create a book chat room." });
 });
 
-app.get('/book-chat-room/:id', async (req, res) => {
-    let book;
-    try {
-        await connectDB()
-        book = await bookModel.findById(req.params.id)
-    } catch (error) {
-        console.log(`ERROR : ${error.message}`)
-        return res.status(400).json({ message: "FAILURE : Get the book chat room." })
-    }
-    return res.status(200).json({ message: "SUCCESS: Get the book chat room.", bookChatRoom: book })
-})
-
-app.get('/book-chat-room-list', async (req, res) => {
-    let bookList;
-    try {
-        await connectDB()
-        bookList = await bookModel.find()
-    } catch (error) {
-        console.log(`ERROR : ${error.message}`)
-        return res.status(400).json({ message: "FAILURE : Get book chat room list." })
-    }
-    return res.status(200).json({ message: "SUCCESS: Get book chat room list.", bookChatRoomList: bookList })
-})
-
-app.delete('/book-chat-room/:id', async (req, res) => {
-    try {
-        await connectDB()
-        await bookModel.deleteOne({ _id: req.params.id })
-    } catch (error) {
-        console.log(`ERROR : ${error.message}`)
-        return res.status(400).json({ message: "FAILURE : Delete the book chat room." })
-    }
-    return res.status(200).json({ message: "SUCCESS: Delete the book chat room." })
-})
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/book-chat-room/:id", async (req, res) => {
+  let book;
+  try {
+    await connectDB();
+    book = await bookModel.findById(req.params.id);
+  } catch (error) {
+    console.log(`ERROR : ${error.message}`);
+    return res
+      .status(400)
+      .json({ message: "FAILURE : Get the book chat room." });
+  }
+  return res
+    .status(200)
+    .json({ message: "SUCCESS: Get the book chat room.", bookChatRoom: book });
 });
 
-io.on('connection', function (socket) {
-
-    let room = '';
-    let name = '';
-
-    io.to(socket.id).emit('connectid', { socketId: socket.id })
-
-    socket.on('login', (msg) => {
-        room = msg.room;
-        socket.join(room);
-        name = msg.name;
-        io.to(room).emit('message', { socketId: `${socket.id}`, name: `${name}`, message: `${name}さんが入室しました。` });
-    })
-    socket.on('message', (msg) => {
-        io.to(room).emit('message', msg);
+app.get("/book-chat-room-list", async (req, res) => {
+  let bookList;
+  try {
+    await connectDB();
+    bookList = await bookModel.find();
+  } catch (error) {
+    console.log(`ERROR : ${error.message}`);
+    return res
+      .status(400)
+      .json({ message: "FAILURE : Get book chat room list." });
+  }
+  return res
+    .status(200)
+    .json({
+      message: "SUCCESS: Get book chat room list.",
+      bookChatRoomList: bookList,
     });
+});
 
-    socket.on('disconnect', () => {
-        io.to(room).emit('message', { socketId: `${socket.id}`, name: `${name}`, message: `${name}さんが退出しました。` })
-    })
+app.delete("/book-chat-room/:id", async (req, res) => {
+  try {
+    await connectDB();
+    await bookModel.deleteOne({ _id: req.params.id });
+  } catch (error) {
+    console.log(`ERROR : ${error.message}`);
+    return res
+      .status(400)
+      .json({ message: "FAILURE : Delete the book chat room." });
+  }
+  return res
+    .status(200)
+    .json({ message: "SUCCESS: Delete the book chat room." });
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function (socket) {
+  let room = "";
+  let name = "";
+
+  io.to(socket.id).emit("connectid", { socketId: socket.id });
+
+  socket.on("login", (msg) => {
+    room = msg.room;
+    socket.join(room);
+    name = msg.name;
+    io.to(room).emit("message", {
+      socketId: `${socket.id}`,
+      name: `${name}`,
+      message: `${name}さんが入室しました。`,
+    });
+  });
+  socket.on("message", (msg) => {
+    io.to(room).emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    io.to(room).emit("message", {
+      socketId: `${socket.id}`,
+      name: `${name}`,
+      message: `${name}さんが退出しました。`,
+    });
+  });
 });
 
 server.listen(port, () => {
-    console.log(`listening on port:${port}`);
+  console.log(`listening on port:${port}`);
 });
