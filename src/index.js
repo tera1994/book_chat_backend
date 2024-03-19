49e0f11eb166b0b4e74e20fa1dc7f4d9ef467b0e
tera <>
2024-03-19 17:20:56 +0900

    Modify url.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://book-chat.netlify.app/",
+    origin: "https://book-chat.netlify.app",
     methods: ["GET", "POST"],
   },
 });
